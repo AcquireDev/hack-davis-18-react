3435a6ae6cd8cce744a61e2c01f830e66101bad0
Ramneek Bains <ramneekbains@MacBook-Pro-2.local>
2018-01-21 06:47:31 -0800

    Add header

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Button } from "react-bootstrap";
-
-import { getApplications } from "../actions/application";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Button } from 'react-bootstrap';
+import { DropdownButton, MenuItem } from 'react-bootstrap';
+import { getApplications } from '../actions/application';
 
 class Dashboard extends Component {
   handleLoadApps = () => {
@@ -22,23 +22,87 @@ class Dashboard extends Component {
 
     return (
       <div>
-        <p>Dashboard</p>
+
+
+        <div
+          id="wrapper"
+          className="Header"
+          style={{
+            backgroundColor: '#5ede5c',
+            height: '7vh',
+            color: 'white',
+            display: 'flex',
+            justifyContent: 'space-between',
+          }}
+        >
+          <div
+            style={{
+              alignItems: 'center',
+              paddingLeft: '0px',
+              display: 'flex',
+              flexBasis: '1px',
+
+            }}
+          >
+            <img src="logo.png" style={{ maxWidth: '18000%', objectFit: 'contain' }} />
+          </div>
+          <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+
+              }}
+          >
+            <p align="center"><DropdownButton
+              bsStyle="default"
+              title="Software Engineer"
+              id="drop1"
+            />
+            </p>
+            <p align="center"><DropdownButton
+              bsStyle="default"
+              title="Intern"
+              id="drop2"
+            />
+            </p>
+            <p align="center"><DropdownButton
+              bsStyle="default"
+              title="Bay Area, CA"
+              id="drop3"
+            />
+            </p>
+          </div>
+          <div
+            style={{
+              paddingRight: '20px',
+              display: 'flex',
+              alignItems: 'center',
+
+            }}
+          >
+            <p align="right"><font size="30">#</font></p>
+          </div>
+
+        </div>
+
         <Button onClick={this.handleLoadApps}>Load Applications</Button>
         {loading}
         <ul>{applicationList}</ul>
+
       </div>
     );
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   const user = state.user;
   const applications = state.applications.applications;
   const appsLoading = state.applications.loadingApps;
   return {
     user,
     applications,
-    appsLoading
+    appsLoading,
   };
 };
 
